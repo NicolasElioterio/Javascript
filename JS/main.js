9d43848a5cd571aea63ad3714dc755de4f0376b6
NicolasElioterio <nicolaselioteriojf@gmail.com>
2021-05-17 15:11:53 -0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -11,3 +11,56 @@ console.log(frase.toLowerCase())
 //alert(frase.replace('Japão','Brasil')
 */
 
+/*
+var lista = ['Maçã','Pêra','Laranja'];
+lista.push('Uva');
+lista.pop();
+console.log(lista[1]);
+console.log(lista.toString()[1]);
+console.log(lista.join(' - '));
+*/
+
+/*
+var fruta = {nome:'maçã',cor:'vermelha'};
+console.log(frutas.nome);
+alert(frutas.cor);
+*/
+
+/*
+var frutas = [{nome:'maçã',cor:'vermelha'}];
+console.log(frutas);
+alert(frutas[1]);
+*/
+
+
+/*
+var idade = prompt('Qual a sua idade? ');
+
+if(idade >= 18){
+    alert('maior de idade')
+}
+else{
+    alert('menor de idade')
+}
+*/
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count)
+    count++ ;
+};
+*/
+
+/*
+var count;
+for (count = 0; count <= 5; count++){
+    alert(count)
+}
+*/
+
+
+var d = new Date();
+alert(d.getDay());
+alert(d.getMonth());
+alert(d.getFullYear());
