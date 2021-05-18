e6c2022a3ada6eff963f752d99d43cdb90d78af9
NicolasElioterio <nicolaselioteriojf@gmail.com>
2021-05-18 10:54:35 -0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -100,3 +100,21 @@ function redirecionar(){
     window.open('https://www.google.com');
     //window.location.href = 'https://www.google.com';
 }
+
+function trocar(elemento){
+    //document.getElementById('mousemove').innerHTML = 'Obrigado por passar o mouse.';
+    elemento.innerHTML = 'Obrigado por passar o mouse.';
+}
+
+function voltar(elemento){
+    //document.getElementById('mousemove').innerHTML = 'Passe o mouse aqui.';
+    elemento.innerHTML = 'Passe o mouse aqui.';
+}
+
+function load(){
+    alert('Página Carregada');
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
