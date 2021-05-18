91c94ba94c766481e4e12b1eced64fb955666689
NicolasElioterio <nicolaselioteriojf@gmail.com>
2021-05-18 15:48:16 -0300

    Update Aula02.js

---
+++ Aula02.js
@@ -0,0 +1,17 @@
+var v1, v2, txt1, txt2;
+
+v1 = 10;
+v2 = 15;
+
+txt1 = 'Olá pessoas';
+txt2 = 'Tchau pessoas';
+
+
+if (v1 != v2){
+    console.log('true');
+    alert(txt1);
+}
+else{
+    console.log('false');
+    alert(txt2);
+}
