80f41d3437ad0866c6e0645679d6a1ba45ed3791
NicolasElioterio <nicolaselioteriojf@gmail.com>
2021-05-18 16:11:08 -0300

    Update Aula03.js

---
+++ Aula03.js
@@ -0,0 +1,9 @@
+var pontos = 72;
+var energia = 60;
+
+if (((pontos >= 30)&&(pontos <= 40)&&(energia >= 40))||((pontos >= 70)&&(pontos <= 80)&&(energia >= 40))){
+    console.log('Ganhou');
+}
+else{
+    console.log('Perdeu')
+}
